a4f8ff82985a71841f4da034991ea8a6ba7ce4d7
Ahuva <58051262+ahuvac@users.noreply.github.com>
2021-03-09 10:07:41 -0500

    Create Circle.cs

---
+++ Downloads/Geometry/GeometryB/Circle.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace GeometryA
+{
+    class Circle : IShape
+    {
+        public Point Center { get; set; }
+        public double Radius { get; set; }
+
+        public Circle(Point center, double radius)
+        {
+            Point Center = new Point();
+            Center.SetCartesian(center.Xcoord(), center.Ycoord());
+            Radius = radius;
+        }
+
+        public double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+        public double GetPerimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+    }
+}
